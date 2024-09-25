ff7310cf1c06a7762d54fe6ef3e7a683c7de04f2
fizzasahar <fizzasahar44>
2024-09-25 19:50:24 +0500

    Portfolio Site

---
+++ app.js
@@ -0,0 +1,9 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
